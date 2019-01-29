8e51b350ebd7c1d2ca71fcc7a67648dedde67ae2
Aldemaro14 <itresearcher14@gmail.com>
2019-01-28 21:06:09 -0600

    initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+
+    state = {
+        test: 'test'
+    };
+
+
+    componentDidMount() {
+      console.log('componentDidMount...');
+    }
+  
+  
+    render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Test;
