c7052af22ddc03a73ddada91fdd91aad308fae36
Aldemaro14 <itresearcher14@gmail.com>
2019-01-28 20:38:54 -0600

    Updates

---
+++ precache-manifest.e6ca4a8795b75dc4ddf1bdd7638d6b05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "f99150f8605bc0ea57ff",
+    "url": "/contactmanager/static/js/main.f99150f8.chunk.js"
+  },
+  {
+    "revision": "2a7d577ecbaa87c74a93",
+    "url": "/contactmanager/static/js/1.2a7d577e.chunk.js"
+  },
+  {
+    "revision": "f99150f8605bc0ea57ff",
+    "url": "/contactmanager/static/css/main.266afd20.chunk.css"
+  },
+  {
+    "revision": "2a7d577ecbaa87c74a93",
+    "url": "/contactmanager/static/css/1.f4b7a165.chunk.css"
+  },
+  {
+    "revision": "9fa27d853c92dc272d38ed0e1aace005",
+    "url": "/contactmanager/index.html"
+  }
+];
